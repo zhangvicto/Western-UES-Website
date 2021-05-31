20cf09537c18a3109d719dd600d2ebd2bd794679
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2021-05-31 00:31:28 -0400

    carousel done

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function fadeIn(element) {
     var op = 0.1;  // initial opacity
     element.style.display = 'block';
     var timer = setInterval(function () {
-        if (op >= 1){
+        if (op >= 1) {
             clearInterval(timer);
         }
         element.style.opacity = op;
@@ -14,7 +14,7 @@ function fadeIn(element) {
 function fadeOut(element) {
     var op = 1;  // initial opacity
     var timer = setInterval(function () {
-        if (op <= 0.1){
+        if (op <= 0.1) {
             clearInterval(timer);
             element.style.display = 'none';
         }
@@ -27,7 +27,7 @@ function fadeOut(element) {
 function topMarginDown(element, dis, time, speed) {
     var distance = 0 + "px";
     var timer = setInterval(function () {
-        if (parseInt(distance) >= dis){
+        if (parseInt(distance) >= dis) {
             clearInterval(timer);
             element.style.paddingTop = dis;
         }
@@ -39,7 +39,7 @@ function topMarginDown(element, dis, time, speed) {
 function topMarginUp(element, dis, time, speed) {
     var distance = dis + "px";
     var timer = setInterval(function () {
-        if (parseInt(distance) <= 0){
+        if (parseInt(distance) <= 0) {
             clearInterval(timer);
             element.style.paddingTop = distance;
         }
@@ -47,3 +47,45 @@ function topMarginUp(element, dis, time, speed) {
         distance = parseInt(distance) - speed + "px";
     }, time);
 }
+
+//Carousel
+//function carousel() {
+//  for (let i = 1; ; i++) {
+//    setTimeout(function () {
+//      let next = i + 1;
+//    console.log("yes")
+//    fadeIn(document.getElementById("slide" + next));
+//    fadeOut(document.getElementById("slide" + i));
+//}, 3000)
+//if (i >= 2) {
+//    i = 0;
+//}
+//}
+//}
+
+//carousel();
+
+var i = 1;
+
+function carousel() {
+    setTimeout(function () {
+        let next = i + 1;
+        if (i != 0) {
+            document.getElementById("slide" + i).style.display = "none";
+        } else {
+            document.getElementById("slide" + 3).style.display = "none";
+        }
+        fadeIn(document.getElementById("slide" + next));
+        if (next == 3) {
+            i = 0;
+            carousel();
+            console.log("ok next")
+        } else {
+            i++;
+            carousel();
+        }
+    }, 3000)
+}
+
+carousel();
+
