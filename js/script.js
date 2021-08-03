8bd0e163ff539d44225e1b820e12f3ed0ee86064
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2021-08-03 17:13:07 -0400

    update about

--- js/script.js
+++ js/script.js
@@ -238,7 +238,7 @@ for (let i = 0; i < mobileMenuItems.length; i++) {
 }
 
 //HIDE MOBILE MENU ON WINDOW RESIZE
-window.addEventListener('resize', function(){
+window.addEventListener('resize', function () {
     fadeOut(mobileMenu);
     buttonState = 0;
     menuButton.classList.remove("open");
@@ -246,45 +246,18 @@ window.addEventListener('resize', function(){
 
 
 //FEE BREAKDOWN
-let labPie = document.getElementById("pie-circle");
+let piePieces = document.getElementsByClassName("pie-piece");
+let pieText = document.getElementsByClassName("pie-middle-text");
+let defaultText = document.getElementById("donation-default");
 
-if (labPie) {
-    labPie.addEventListener("mouseover", function () {
-        fadeIn(document.getElementById("lab-text"));
+for (let i = 0; i < piePieces.length; i++) {
+    piePieces[i].addEventListener("mouseover", function () {
+        fadeIn(pieText[i]);
+        fadeOut(defaultText);
     });
-}
-
-if (labPie) {
-    labPie.addEventListener("mouseout", function () {
-        fadeOut(document.getElementById("lab-text"));
-    });
-}
-
-let projectPie = document.getElementById("pie-circle");
-
-if (projectPie) {
-    projectPie.addEventListener("click", function () {
-        fadeIn(document.getElementById("lab-text"));
-    });
-}
-
-if (projectPie) {
-    projectPie.addEventListener("mouseout", function () {
-        fadeOut(document.getElementById("lab-text"));
-    });
-}
-
-let uesPie = document.getElementById("pie-lab");
-
-if (uesPie) {
-    uesPie.addEventListener("click", function () {
-        fadeIn(document.getElementById("lab-text"));
-    });
-}
-
-if (uesPie) {
-    uesPie.addEventListener("mouseout", function () {
-        fadeOut(document.getElementById("lab-text"));
+    piePieces[i].addEventListener("mouseleave", function () {
+        fadeOut(pieText[i]);
+        fadeIn(defaultText);
     });
 }
 
@@ -295,130 +268,132 @@ let galleryClose = document.getElementById("gallery-close");
 
 let gallery1 = document.getElementById("gallery-1");
 
-galleryButton1.addEventListener("click", function () {
-    fadeIn(gallery1);
-});
-
-galleryClose.addEventListener("click", function () {
-    fadeOut(gallery1);
-})
+if (galleryButton1) {
+    galleryButton1.addEventListener("click", function () {
+        fadeIn(gallery1);
+    });
+    galleryClose.addEventListener("click", function () {
+        fadeOut(gallery1);
+    })
+}
 
 
 //GALLERY
+if (document.getElementsByClassName("jacket-gallery-photo")[0]) {
+    !(function (d) {
+        let itemClassName = "jacket-gallery-photo";
+        let items = d.getElementsByClassName(itemClassName);
+        let totalItems = items.length;
+        let slide = 0;
+        let moving = true;
 
-!(function(d){
-let itemClassName = "jacket-gallery-photo";
-let items = d.getElementsByClassName(itemClassName);
-let totalItems = items.length;
-let slide = 0;
-let moving = true;
+        // Set classes
+        function setInitialClasses() {
+            // Targets the previous, current, and next items
+            // This assumes there are at least three items.
+            items[totalItems - 1].classList.add("prev");
+            items[0].classList.add("active");
+            items[1].classList.add("next");
+        }
 
-// Set classes
-function setInitialClasses() {
-    // Targets the previous, current, and next items
-    // This assumes there are at least three items.
-    items[totalItems - 1].classList.add("prev");
-    items[0].classList.add("active");
-    items[1].classList.add("next");
-}
+        // Set event listeners
+        function setEventListeners() {
+            let next = d.getElementsByClassName("jacket-gallery-button--next")[0];
+            let prev = d.getElementsByClassName("jacket-gallery-button--prev")[0];
+            next.addEventListener("click", function () { moveNext() });
+            prev.addEventListener("click", function () { movePrev() });
+        }
 
-// Set event listeners
-function setEventListeners() {
-    let next = d.getElementsByClassName("jacket-gallery-button--next")[0];
-    let prev = d.getElementsByClassName("jacket-gallery-button--prev")[0];
-    next.addEventListener("click", function() {moveNext()});
-    prev.addEventListener("click", function() {movePrev()});
-}
+        function disableInteraction() {
+            // Set 'moving' to true for the same duration as our transition.
+            // (0.5s = 500ms)
 
-function disableInteraction() {
-    // Set 'moving' to true for the same duration as our transition.
-    // (0.5s = 500ms)
+            moving = true;
+            // setTimeout runs its function once after the given time
+            setTimeout(function () {
+                moving = false
+            }, 500);
+        }
 
-    moving = true;
-    // setTimeout runs its function once after the given time
-    setTimeout(function () {
-        moving = false
-    }, 500);
-}
+        function moveCarouselTo(slide) {
+            // Check if carousel is moving, if not, allow interaction
+            if (!moving) {
+                // temporarily disable interactivity
+                disableInteraction();
+                // Update the "old" adjacent slides with "new" ones
+                let newPrevious = slide - 1,
+                    newNext = slide + 1,
+                    oldPrevious = slide - 2,
+                    oldNext = slide + 2;
+                // Test if carousel has more than three items
+                if ((totalItems - 1) > 3) {
+                    // Checks and updates if the new slides are out of bounds
+                    if (newPrevious <= 0) {
+                        oldPrevious = (totalItems - 1);
+                    } else if (newNext >= (totalItems - 1)) {
+                        oldNext = 0;
+                    }
+                    // Checks and updates if slide is at the beginning/end
+                    if (slide === 0) {
+                        newPrevious = (totalItems - 1);
+                        oldPrevious = (totalItems - 2);
+                        oldNext = (slide + 1);
+                    } else if (slide === (totalItems - 1)) {
+                        newPrevious = (slide - 1);
+                        newNext = 0;
+                        oldNext = 1;
+                    }
 
-function moveCarouselTo(slide) {
-    // Check if carousel is moving, if not, allow interaction
-    if (!moving) {
-        // temporarily disable interactivity
-        disableInteraction();
-        // Update the "old" adjacent slides with "new" ones
-        let newPrevious = slide - 1,
-            newNext = slide + 1,
-            oldPrevious = slide - 2,
-            oldNext = slide + 2;
-        // Test if carousel has more than three items
-        if ((totalItems - 1) > 3) {
-            // Checks and updates if the new slides are out of bounds
-            if (newPrevious <= 0) {
-                oldPrevious = (totalItems - 1);
-            } else if (newNext >= (totalItems - 1)) {
-                oldNext = 0;
+                    items[oldPrevious].className = itemClassName;
+                    items[oldNext].className = itemClassName;
+                    // Add new classes
+                    items[newPrevious].className = itemClassName + " prev";
+                    items[slide].className = itemClassName + " active";
+                    items[newNext].className = itemClassName + " next";
+                }
             }
-            // Checks and updates if slide is at the beginning/end
-            if (slide === 0) {
-                newPrevious = (totalItems - 1);
-                oldPrevious = (totalItems - 2);
-                oldNext = (slide + 1);
-            } else if (slide === (totalItems - 1)) {
-                newPrevious = (slide - 1);
-                newNext = 0;
-                oldNext = 1;
+        }
+
+
+        // Next navigation handler
+        function moveNext() {
+            // Check if moving
+            if (!moving) {
+                // If it's the last slide, reset to 0, else +1
+                if (slide === (totalItems - 1)) {
+                    slide = 0;
+                } else {
+                    slide++;
+                }
+                // Move carousel to updated slide
+                moveCarouselTo(slide);
             }
-
-            items[oldPrevious].className = itemClassName;
-            items[oldNext].className = itemClassName;
-            // Add new classes
-            items[newPrevious].className = itemClassName + " prev";
-            items[slide].className = itemClassName + " active";
-            items[newNext].className = itemClassName + " next";
-        }
-    }
-}
-
-
-// Next navigation handler
-function moveNext() {
-    // Check if moving
-    if (!moving) {
-        // If it's the last slide, reset to 0, else +1
-        if (slide === (totalItems - 1)) {
-            slide = 0;
-        } else {
-            slide++;
-        }
-        // Move carousel to updated slide
-        moveCarouselTo(slide);
-    }
-}
-
-// Previous navigation handler
-function movePrev() {
-    // Check if moving
-    if (!moving) {
-        // If it's the first slide, set as the last slide, else -1
-        if (slide === 0) {
-            slide = (totalItems - 1);
-        } else {
-            slide--;
         }
 
-        // Move carousel to updated slide
-        moveCarouselTo(slide);
-    }
+        // Previous navigation handler
+        function movePrev() {
+            // Check if moving
+            if (!moving) {
+                // If it's the first slide, set as the last slide, else -1
+                if (slide === 0) {
+                    slide = (totalItems - 1);
+                } else {
+                    slide--;
+                }
+
+                // Move carousel to updated slide
+                moveCarouselTo(slide);
+            }
+        }
+
+        function initCarousel() {
+            setInitialClasses();
+            setEventListeners();
+            // Set moving to false so that the carousel becomes interactive
+            moving = false;
+        }
+
+        initCarousel();
+
+    }(document));
 }
-
-function initCarousel() {
-    setInitialClasses();
-    setEventListeners();
-    // Set moving to false so that the carousel becomes interactive
-    moving = false;
-}
-
-initCarousel();
-
-}(document));
