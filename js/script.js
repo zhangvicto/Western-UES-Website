5e479040cb0ebca19e4de913592d3be5faa0a3e1
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2021-08-05 17:23:26 -0400

    Oweek Stuff and countdown

--- js/script.js
+++ js/script.js
@@ -99,6 +99,38 @@ if (document.getElementById("slide1")) {
     carousel();
 }
 
+(function () {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    let birthday = "Sep 9, 2021 00:00:00",
+        countDown = new Date(birthday).getTime(),
+        x = setInterval(function () {
+
+            let now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+            //do something later when date is reached
+            if (distance < 0) {
+                let headline = document.getElementById("headline"),
+                    countdown = document.getElementById("countdown");
+
+                headline.innerText = "Oweek is here!";
+                countdown.style.display = "none";
+
+                clearInterval(x);
+            }
+            //seconds
+        }, 0)
+}());
+
 
 //if click on circle then stop carousel
 // function stopCarousel() {
